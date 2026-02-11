52479286b32775a23121797bad446f96ae1dd856
Emmanuel Macías Silva <emmanuelelegantini@gmail.com>
2026-02-11 09:38:20 -0600

    feat: add reporteController

---
+++ src/controllers/reporteController.js
@@ -0,0 +1,40 @@
+const reporte = require('../models/reporte')
+
+// Get all reports
+// req = request body {} params url_param1=datos123
+// res = response
+exports.getReportes = async (req, res) => {
+    try {
+        const reportes = await reporte.find();
+        res.json(reportes);
+    } catch (error) {
+        res.status(500).json({error: "Error: Get Reports", message: error});
+    }
+}
+
+//Create new report
+exports.createReporte = async (req, res) => {
+    try {
+        const {titulo, descripcion, ubicacion} = req.body;
+
+        //Logic
+        let prioridad = "media";
+        if(descripcion.toLowerCase().includes('fuego') || 
+        descripcion.toLowerCase().includes('incendio')
+        )   {
+            prioridad = "alta"
+        }
+
+        const nuevoReporte = new Reporte({
+            titulo,
+            descripcion,
+            ubicacion,
+            prioridad
+        });
+
+        await nuevoReporte.save();
+        res.status(201).json(nuevoReporte)
+    } catch (error) {
+        res.status(500).json({error: "Error: Create reports", message: error})
+    }
+}
